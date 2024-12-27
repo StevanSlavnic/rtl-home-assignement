5032594d406b844dbe7598224056399d8a980b67
Stevan <slavnic.stevan@gmail.com>
2024-12-27 20:55:44 +0100

    Fetch articles in Sport component using PAGE_LIMIT constant, setup NEXT_PUBLIC_API_URL in  env enviorment

--- app/sport/page.tsx
+++ app/sport/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import fetchArticles from "../actions/actions";
+import { PAGE_LIMIT } from "../constants";
 
-export default function Sport() {
+export default async function Sport() {
+  const { data: articles } = await fetchArticles(1, PAGE_LIMIT);
+  console.log("articles :", articles);
+
   return <div>Sport</div>;
 }
